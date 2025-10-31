9d5788e36fd8abe53e3cfe94640678fa65487091
Irina1234567890 <43700354+Irina1234567890@users.noreply.github.com>
2025-10-31 06:17:04 +0200

    git command and terminal

---
+++ byte-education/bemodule1.js
@@ -0,0 +1,57 @@
+console.log('Hello world!');
+console.log(1);
+let test = 'Hi!'
+console.log(test);
+
+
+//command git
+//pwd-tekywyu derectoriyu
+//cd-you can change directory
+
+//mkdir -sozdanie papki
+//mkdir test
+
+//touch-sozdat file
+//touch hello.txt
+
+//ls-vivodit vse files v tvoeu directorii
+//ls -1 -vivedet files v stolbik
+//ls -a -vivesti vsefiles directory
+//ls -1a -vivedet vse files directory v stolbik
+//get --version -versiya git
+
+//git init -inicializiroval repozotoriu
+
+//git status - pokazyet status repozitoriya
+
+//git commit -m ''  -sozdaem commit
+
+//git add .  -dobavit vse files, kotorie sywestvyyut v tekyweu directorii
+//git add test.txt -dobavlyaem opredelenniu file
+
+//git diff -pokazivaet raznicy megdy commitami i pokazivaet, chto izmenilos
+
+//git log -istoriya commitov
+
+//git checkout ykaziveem hesh comita - mogem posmotret' commit i chto bilo v nem
+//git checkout master -vozvrashaet na nashy vetky
+//git checkout - vozvrawaet na shag nazad
+
+//nastraivaem okrygenie git
+//git config --global user.name  -napisat' imya, kotorim bydyt podpisani comiti
+//git config --global user.email = -zapisivaem email na kotoriu zaregistrirovan git hub, chob ne bilo problem
+
+//privyajem lokal'niu repozitoriu k ydalennomy
+//git remote add origin adress repozitoriya
+
+//git branch -M main dlya pereimenovke
+
+//zapushit' v git.hub
+//git push - u origin main -takyu zapis' delaete, kogda perviu raz pushite
+//moget zaprosit' parol  i username ot github
+
+//mogno files zabirat' ili clonirovat', ili peretaskivat'
+//clone repozitor:
+//kopiryem ssilky -> git clone ssilka kotoryyu skopirovali->enter
+
+
